bb2028cc970f32132959019a95fc813d4c12d41c
Benedict Thompson <git@benjft.uk>
2025-06-29 23:53:25 +0100

    Introduced tests and coverage tracking

---
+++ Benjft.Util.DependencyInjection.Tests/Usings.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Linq;
+global using System.Reflection;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Xunit;
+
+// Test-only namespaces
+global using Benjft.Util.DependencyInjection;
+global using Benjft.Util.DependencyInjection.Attributes;
+global using Benjft.Util.DependencyInjection.Extensions;
+global using Benjft.Util.DependencyInjection.Exceptions;
