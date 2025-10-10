2fc55e928efc4702b4952846533afc4c8ad1f7f2
Benedict Thompson <git@benjft.uk>
2025-10-10 18:54:07 +0100

    Add descriptive XML documentation for exception classes in DependencyInjection namespace

--- Benjft.Util.DependencyInjection/Exceptions/InvalidFactoryMethodException.cs
+++ Benjft.Util.DependencyInjection/Exceptions/InvalidFactoryMethodException.cs
@@ -1,5 +1,9 @@
 ﻿namespace Benjft.Util.DependencyInjection.Exceptions;
 
+/// <summary>
+/// Represents errors that occur when a method marked as a service factory is invalid.
+/// This is the base class for more specific factory method exceptions.
+/// </summary>
 public class InvalidFactoryMethodException : DependencyInjectionAttributeException {
     internal InvalidFactoryMethodException(string? message) 
         : base(message) { }
